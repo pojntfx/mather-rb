c2e5c53e357376ed7e8d58c66657d865b05b8edd
Felix Pojtinger <felix@pojtinger.com>
2019-11-06 20:02:18 +0100

    feat: Handle errors and switch to clean architecture

---
+++ src/svc/mather.rb
@@ -0,0 +1,49 @@
+this_dir = File.expand_path(File.dirname(__FILE__))
+proto_dir = File.join(this_dir, 'proto')
+lib_dir = File.join(this_dir, '..', 'lib')
+$LOAD_PATH.unshift(proto_dir)
+$LOAD_PATH.unshift(lib_dir)
+
+require 'mather_services_pb'
+require 'grpc'
+require 'mather'
+
+class MatherService < Mather::Math::Service
+  def add(req, _)
+    if req.First == 0
+      raise GRPC::BadStatus.new(
+              GRPC::Core::StatusCodes::INVALID_ARGUMENT,
+              'could not add, `First` has not been provided'
+            )
+    end
+
+    if req.Second == 0
+      raise GRPC::BadStatus.new(
+              GRPC::Core::StatusCodes::INVALID_ARGUMENT,
+              'could not add, `Second` has not been provided'
+            )
+    end
+
+    Mather::MathAddReply.new(Result: MatherLib.new.add(req.First, req.Second))
+  end
+
+  def subtract(req, _)
+    if req.First == 0
+      raise GRPC::BadStatus.new(
+              GRPC::Core::StatusCodes::INVALID_ARGUMENT,
+              'could not add, `First` has not been provided'
+            )
+    end
+
+    if req.Second == 0
+      raise GRPC::BadStatus.new(
+              GRPC::Core::StatusCodes::INVALID_ARGUMENT,
+              'could not add, `Second` has not been provided'
+            )
+    end
+
+    Mather::MathSubtractReply.new(
+      Result: MatherLib.new.subtract(req.First, req.Second)
+    )
+  end
+end
