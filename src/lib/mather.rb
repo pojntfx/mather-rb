71c3e46ab56444f21eb74c79c12d88ac58eeac42
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-11-11 23:15:10 +0100

    docs: Add standardized documentation

--- src/lib/mather.rb
+++ src/lib/mather.rb
@@ -1,8 +1,10 @@
 class MatherLib
+  # add adds two numbers
   def add(first, second)
     return first + second
   end
 
+  # subtract subtracts two numbers
   def subtract(first, second)
     return second - first
   end
